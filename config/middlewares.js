ff66e6405174eae9eebbb5ca8b0e3163280e64a1
binhvone <binhvo1311217@gmail.com>
2024-07-26 23:36:08 +0700

    fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,16 @@
 module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
