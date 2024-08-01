8ceb4f3b4c3bc4002cadecec35478e9d7cf89922
binhvone <binhvone@gmail.com>
2024-08-01 08:29:55 +0700

    fix blog

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -3,7 +3,32 @@
 /**
  * blog controller
  */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::blog.blog');
+module.exports = createCoreController('api::blog.blog', ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+
+    const query = strapi.db.query('api::blog.blog');
+
+    await Promise.all(
+      data.map(async (item, index) => {
+        const blog = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['createdBy'],
+        });
+        
+        data[index].attributes.createdBy = {
+          id: blog.createdBy.id,
+          firstname: blog.createdBy.firstname,
+          lastname: blog.createdBy.lastname,
+        };
+      })
+    );
+
+    return { data, meta };
+  },
+}));
